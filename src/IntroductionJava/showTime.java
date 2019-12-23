052c01d2a51280c81584a41f35777fc4d9f8d948
= <=>
2019-12-23 13:47:27 +0700

    bai tap java

---
+++ src/IntroductionJava/showTime.java
@@ -0,0 +1,10 @@
+package IntroductionJava;
+
+import java.util.Date;
+
+public class showTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("date now : " + now);
+    }
+}
